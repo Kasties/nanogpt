48bc4bb2a103c1fe94fc0e94a9272dce9d5ae1cf
Kasties <kamsties10@gmail.com>
2026-02-19 18:01:05 +0000

    added fineweb download

--- temp.py
+++
@@ -1,5 +0,0 @@
-import jax
-key = jax.random.PRNGKey(1337)
-print(key)
-keys = jax.random.split(key=key, num=20) # We need a lot of random keys, so we split the key into many subkeys at once
-print(keys[0])
